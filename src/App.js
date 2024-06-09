54baee800cb7c7c398f6abb35886e7b5c4ab501f
Fredrik <fredrikf2000@gmail.com>
2024-06-09 19:53:24 +0200

    added axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "api_key=e0578aab27ca56d455e61fc6ca80b6b7";
+import axios from "axios";
+
+let SEARCH_URL =
+  BASE_URL + "search/movie?" + API_KEY + "&sort_by=popularity.desc&query=";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [show, setShow] = useState([]);
+  return <div className="App"></div>;
 }
 
 export default App;
